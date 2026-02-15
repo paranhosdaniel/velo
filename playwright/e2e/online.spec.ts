206773cea1d90f2451ce612ae68fc5fd144a249a
Daniel Paranhos <danielcparanhos@gmail.com>
2026-02-15 12:26:15 -0300

    Changing the title back to original

--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('The WebApp should be online', async ({ page }) => {
+test('WebApp should be online', async ({ page }) => {
   await page.goto('http://localhost:5173/');
 
   await expect(page).toHaveTitle(/Velô by Papito/);
